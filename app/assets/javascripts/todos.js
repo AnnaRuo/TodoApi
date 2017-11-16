18aa173354e826ed85fa73945437e35d53f595b8
Anna <anna.ruotanen@gmail.com>
2017-11-16 16:01:56 +0100

    Create todos via ajax

--- app/assets/javascripts/todos.js
+++ app/assets/javascripts/todos.js
@@ -69,7 +69,8 @@ function createTodo(title) {
     console.log(error)
     error_message = error.responseJSON.title[0];
     showError(error_message);
-  })
+  });
+}
 
   function showError(message) {
     var errorHelpBlock = $('<span class="help-block"></span>')
@@ -80,7 +81,6 @@ function createTodo(title) {
       .addClass("has-error")
       .append(errorHelpBlock);
   }
-}
 
 
 function resetErrors() {
@@ -108,4 +108,57 @@ $(document).ready(function() {
   $("form").bind('submit', submitTodo);
   $("#clean-up").bind('click', cleanUpDoneTodos);
   updateCounters();
-});
+})
+
+//new
+
+function toggleDone() {
+  $(this).parent().parent().toggleClass("success");
+  updateCounters();
+}
+
+function updateCounters() {
+  $("#total-count").html($(".todo").size());
+  $("#completed-count").html($(".success").size());
+  $("#todo-count").html($(".todo").size() - $(".success").size());
+}
+
+function createTodo(title) {
+  var checkboxId = "todo-" + nextTodoId();
+
+  var label = $('<label></label>')
+    .attr('for', checkboxId)
+    .html(title);
+
+  var checkbox = $('<input type="checkbox" value="1" />')
+    .attr('id', checkboxId)
+    .bind('change', toggleDone);
+
+  var tableRow = $('<tr class="todo"></td>')
+    .append($('<td>').append(checkbox))
+    .append($('<td>').append(label));
+
+  $("#todoList").append( tableRow );
+
+  updateCounters();
+}
+
+function submitTodo(event) {
+  event.preventDefault();
+  createTodo($("#todo_title").val());
+  $("#todo_title").val(null);
+  updateCounters();
+}
+
+function cleanUpDoneTodos(event) {
+  event.preventDefault();
+  $.when($(".success").remove())
+    .then(updateCounters);
+}
+
+$(document).ready(function() {
+  $("input[type=checkbox]").bind('change', toggleDone);
+  $("form").bind('submit', submitTodo);
+  $("#clean-up").bind('click', cleanUpDoneTodos);
+  updateCounters();
+})
