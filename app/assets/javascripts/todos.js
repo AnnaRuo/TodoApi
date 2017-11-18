28a9490a7509d91fe381a06c6ac126f8b007cc67
Anna <anna.ruotanen@gmail.com>
2017-11-18 18:36:46 +0100

    Fixed create, update and delete todos via Ajax

--- app/assets/javascripts/todos.js
+++ app/assets/javascripts/todos.js
@@ -1,3 +1,5 @@
+// app/assets/javascripts/todos.js
+
 function toggleDone() {
   var checkbox = this;
   var tableRow = $(this).parent().parent();
@@ -23,6 +25,7 @@ function toggleDone() {
   });
 }
 
+
 function updateCounters() {
   $("#total-count").html($(".todo").size());
   $("#completed-count").html($(".success").size());
@@ -41,7 +44,6 @@ function createTodo(title) {
     contentType: "application/json",
     dataType: "json"
   })
-
   .done(function(data) {
     console.log(data);
 
@@ -64,46 +66,6 @@ function createTodo(title) {
 
     updateCounters();
   })
-
-  .fail(function(error) {
-    console.log(error)
-    error_message = error.responseJSON.title[0];
-    showError(error_message);
-  });
-}
-  function showError(message) {
-    var errorHelpBlock = $('<span class="help-block"></span>')
-      .attr('id', 'error_message')
-      .text(message);
-
-    $("#formgroup-title")
-      .addClass("has-error")
-      .append(errorHelpBlock);
-  }
-
-  .done(function(data) {
-    console.log(data);
-
-    var checkboxId = data.id;
-
-    var label = $('<label></label>')
-      .attr('for', checkboxId)
-      .html(title);
-
-    var checkbox = $('<input type="checkbox" value="1" />')
-      .attr('id', checkboxId)
-      .bind('change', toggleDone);
-
-    var tableRow = $('<tr class="todo"></td>')
-      .attr('data-id', checkboxId)
-      .append($('<td>').append(checkbox))
-      .append($('<td>').append(label));
-
-    $("#todoList").append(tableRow);
-
-    updateCounters();
-  })
-
 
   .fail(function(error) {
     console.log(error)
@@ -117,10 +79,19 @@ function resetErrors() {
   $("#formgroup-title").removeClass("has-error");
 }
 
+function showError(message) {
+  var errorHelpBlock = $('<span class="help-block"></span>')
+    .attr('id', 'error_message')
+    .text(message);
+
+  $("#formgroup-title")
+    .addClass("has-error")
+    .append(errorHelpBlock);
+}
+
 function submitTodo(event) {
   event.preventDefault();
   resetErrors();
-
   createTodo($("#todo_title").val());
   $("#todo_title").val(null);
   updateCounters();
@@ -143,47 +114,9 @@ function deleteTodo(todoId) {
     dataType: "json"
   })
   .done(function(data) {
-    $('tr[data-id="'+todoId+'"]').remove();
+    $('tr[data-id="' + todoId + '"]').remove();
     updateCounters();
   });
-}
-
-$(document).ready(function() {
-  $("input[type=checkbox]").bind('change', toggleDone);
-  $("form").bind('submit', submitTodo);
-  $("#clean-up").bind('click', cleanUpDoneTodos);
-  updateCounters();
-})
-
-//new
-
-function updateCounters() {
-  $("#total-count").html($(".todo").size());
-  $("#completed-count").html($(".success").size());
-  $("#todo-count").html($(".todo").size() - $(".success").size());
-}
-
-function showError(message) {
-  var errorHelpBlock = $('<span class="help-block"></span>')
-    .attr('id', 'error_message')
-    .text(message);
-
-  $("#formgroup-title")
-    .addClass("has-error")
-    .append(errorHelpBlock);
-}
-
-function submitTodo(event) {
-  event.preventDefault();
-  createTodo($("#todo_title").val());
-  $("#todo_title").val(null);
-  updateCounters();
-}
-
-function cleanUpDoneTodos(event) {
-  event.preventDefault();
-  $.when($(".success").remove())
-    .then(updateCounters);
 }
 
 $(document).ready(function() {
